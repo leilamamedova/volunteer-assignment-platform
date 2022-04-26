90bcfe6be49438d9f59f7f14a8d4d933b8b81eab
leilamamedova <leila.m26ova@gmail.com>
2022-04-26 12:46:17 +0400

    change components  name

---
+++ src/pages/SearchAndAssign/SearchAndAssign.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SearchAndAssign.scss';
+
+const SearchAndAssign =  () => {
+    return(
+        <p>Search and Assign</p>
+    )
+};
+
+export default SearchAndAssign;
