6c485079087fc25aba34ebbd3c1fcbce55542cbd
leilamamedova <leila.m26ova@gmail.com>
2022-04-26 15:36:46 +0400

    create assigning to  component on search assign page

---
+++ src/components/AssigningTo/AssigningTo.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Space, Select, Card  } from 'antd';
+import './AssigningTo.scss';
+
+const { Option } = Select;
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+
+const AssigningTo = () => {
+    return (
+        <div>
+            <Space direction='horizontal' className='assignin-to-component'>
+                <Space direction='vertical'>
+                    <Select defaultValue="default" onChange={handleChange}>
+                        <Option value="default">FA</Option>
+                        <Option value="1">One</Option>
+                        <Option value="2">Two</Option>
+                    </Select>  
+
+                    <Select defaultValue="default" onChange={handleChange}>
+                        <Option value="default">Role</Option>
+                        <Option value="1">One</Option>
+                        <Option value="2">Two</Option>
+                    </Select>  
+
+                    <Select defaultValue="default" onChange={handleChange}>
+                        <Option value="default">Venue</Option>
+                        <Option value="1">One</Option>
+                        <Option value="2">Two</Option>
+                    </Select>                        
+                </Space>
+
+                <Card size='small'>
+                    <p>Role Fulfillment</p>
+                    <p>90 out of 100</p>
+                    <p>90%</p>
+                </Card>
+            </Space>
+        </div>
+    );
+};
+
+export default AssigningTo;
