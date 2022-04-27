185fc90bf16293b8b03e138d7e60c3ef29ce1f04
Musa Musazada <musaam@code.edu.az>
2022-04-27 15:27:24 +0400

    ./SearchAndAssign.js Components has been divided into 2-1 Layout

--- src/components/AssignSearchResult/AssignSearchResult.js
+++ src/components/AssignSearchResult/AssignSearchResult.js
@@ -1,75 +1,79 @@
-import React  from 'react';
-import { Button, Space, Table, Tooltip  } from 'antd';
-import { CloseCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
-import './AssignSearchResult.scss';
+import React from "react";
+import { Button, Space, Table, Tooltip } from "antd";
+import { CloseCircleOutlined, CheckCircleOutlined } from "@ant-design/icons";
+import "./AssignSearchResult.scss";
 
 const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-    },
-    {
-        title: <a>Assign all</a>,
-        key: 'assign',
-        render: () => (
-            <Space className='action-icons'>
-                <Tooltip title="Assign">
-                    <Button icon={<CheckCircleOutlined />}/>
-                </Tooltip>
-            </Space>
-        ),
-    },
-    {
-        title: <a>Deassign all</a>,
-        key: 'deassign',
-        render: () => (
-            <Space className='action-icons'>
-                <Tooltip title="Deassign">
-                    <Button icon={<CloseCircleOutlined />}/>
-                </Tooltip>
-            </Space>
-        ),
-    },
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+  },
+  {
+    title: <a>Assign all</a>,
+    key: "assign",
+    render: () => (
+      <Space className="action-icons">
+        <Tooltip title="Assign">
+          <Button icon={<CheckCircleOutlined />} />
+        </Tooltip>
+      </Space>
+    ),
+  },
+  {
+    title: <a>Deassign all</a>,
+    key: "deassign",
+    render: () => (
+      <Space className="action-icons">
+        <Tooltip title="Deassign">
+          <Button icon={<CloseCircleOutlined />} />
+        </Tooltip>
+      </Space>
+    ),
+  },
 ];
 
 const data = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-]; 
-  
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+  },
+];
+
 const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    }
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(
+      `selectedRowKeys: ${selectedRowKeys}`,
+      "selectedRows: ",
+      selectedRows
+    );
+  },
 };
 
 const AssignSearchResult = () => {
   return (
-    <div className='assign-search-result'>
+    <div className="assign-search-result">
       <Table
         rowSelection={{
           ...rowSelection,
--- src/components/AssigningTo/AssigningTo.js
+++ src/components/AssigningTo/AssigningTo.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Space, Select, Card  } from 'antd';
-import './AssigningTo.scss';
+import React from "react";
+import { Space, Select, Card } from "antd";
+import "./AssigningTo.scss";
 
 const { Option } = Select;
 
@@ -9,37 +9,37 @@ function handleChange(value) {
 }
 
 const AssigningTo = () => {
-    return (
-        <div>
-            <Space direction='horizontal' className='assignin-to-component card'>
-                <Space direction='vertical'>
-                    <Select defaultValue="default" onChange={handleChange}>
-                        <Option value="default">FA</Option>
-                        <Option value="1">One</Option>
-                        <Option value="2">Two</Option>
-                    </Select>  
+  return (
+    <div style={{ width: "100%" }}>
+      <Space direction="horizontal" className="assignin-to-component card">
+        <Space direction="vertical">
+          <Select defaultValue="default" onChange={handleChange}>
+            <Option value="default">FA</Option>
+            <Option value="1">One</Option>
+            <Option value="2">Two</Option>
+          </Select>
 
-                    <Select defaultValue="default" onChange={handleChange}>
-                        <Option value="default">Role</Option>
-                        <Option value="1">One</Option>
-                        <Option value="2">Two</Option>
-                    </Select>  
+          <Select defaultValue="default" onChange={handleChange}>
+            <Option value="default">Role</Option>
+            <Option value="1">One</Option>
+            <Option value="2">Two</Option>
+          </Select>
 
-                    <Select defaultValue="default" onChange={handleChange}>
-                        <Option value="default">Venue</Option>
-                        <Option value="1">One</Option>
-                        <Option value="2">Two</Option>
-                    </Select>                        
-                </Space>
+          <Select defaultValue="default" onChange={handleChange}>
+            <Option value="default">Venue</Option>
+            <Option value="1">One</Option>
+            <Option value="2">Two</Option>
+          </Select>
+        </Space>
 
-                <Card size='small'>
-                    <p>Role Fulfillment</p>
-                    <p>90 out of 100</p>
-                    <p>90%</p>
-                </Card>
-            </Space>
-        </div>
-    );
+        <Card size="small">
+          <p>Role Fulfillment</p>
+          <p>90 out of 100</p>
+          <p>90%</p>
+        </Card>
+      </Space>
+    </div>
+  );
 };
 
 export default AssigningTo;
