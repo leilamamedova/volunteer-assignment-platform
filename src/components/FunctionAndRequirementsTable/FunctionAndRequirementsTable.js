2ea6bf620bf8bbb4f46f25c8880bce4f57744ad0
leilamamedova <leila.m26ova@gmail.com>
2022-05-02 18:42:55 +0400

    create modal on functional requirement page, create store

--- src/components/FunctionAndRequirementsTable/FunctionAndRequirementsTable.js
+++ src/components/FunctionAndRequirementsTable/FunctionAndRequirementsTable.js
@@ -1,87 +1,102 @@
-import React from "react";
-import { Button, Space, Table, Tag } from "antd";
+import React, { useState } from "react";
+import { Button, Space, Table, Tag, InputNumber } from "antd";
 import { EditOutlined } from "@ant-design/icons";
+import FunctionAndRequirementsModal from "./components/FunctionAndRequirementsModal/FunctionAndRequirementsModal";
 import "./FunctionAndRequirementsTable.scss";
 
-const columns = [
-  {
-    title: "Venue",
-    dataIndex: "venue",
-  },
-  {
-    title: "Functional Area",
-    dataIndex: "functional_area",
-  },
-  {
-    title: "Job Title",
-    dataIndex: "job_title",
-  },
-  {
-    title: "Functional Requirements",
-    dataIndex: "functional_requirements",
-    render: tags => (
-      <>
-        {tags.map(tag => {
-          return (
-            <Tag key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
-      </>
-    ),
-  },
-  {
-    title: "Headcount",
-    dataIndex: "headcount",
-  },
-  {
-    title: 'Action',
-    key: "action",
-    render: () => (
-        <Space className="action-icons">
-            <Button icon={<EditOutlined />} />
-        </Space>
-    ),
-  },
-];
+const FunctionAndRequirementsTable = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
-const data = [
-  {
-    key: "1",
-    venue: "Lorem Ipsum",
-    functional_area: 'Lorem Ipsum',
-    job_title: "Lorem Ipsum",
-    functional_requirements: ['Lorem', 'Ipsum'],
-    headcount: 100
-  },
-  {
-    key: "2",
-    venue: "Lorem Ipsum",
-    functional_area: 'Lorem Ipsum',
-    job_title: "Lorem Ipsum",
-    functional_requirements: ['Lorem', 'Ipsum'],
-    headcount: 100
-  },
-  {
-    key: "3",
-    venue: "Lorem Ipsum",
-    functional_area: 'Lorem Ipsum',
-    job_title: "Lorem Ipsum",
-    functional_requirements: ['Lorem', 'Ipsum'],
-    headcount: 100
-  },
-];
+  const handleHeadcount = (value) => {
+    console.log(value);
+  }
 
-const FARequirementsTable = () => {
+  const handleModal = (key) => {
+    console.log(key);
+    setIsModalVisible(true)
+  }
+
+  const columns = [
+    {
+      title: "Venue",
+      dataIndex: "venue",
+    },
+    {
+      title: "Functional Area",
+      dataIndex: "functional_area",
+    },
+    {
+      title: "Job Title",
+      dataIndex: "job_title",
+    },
+    {
+      title: "Headcount",
+      dataIndex: "headcount",
+    },
+    {
+      title: "Functional Requirements",
+      dataIndex: "functional_requirements",
+      render: tags => (
+        <>
+          {tags.map(tag => {
+            return (
+              <Tag key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: "action",
+      render: (_, record) => (
+          <Space className="action-icons">
+              <Button icon={<EditOutlined />} onClick={() =>  handleModal(record.key)} />
+          </Space>
+      ),
+    },
+  ];
+  
+  const data = [
+    {
+      key: "1",
+      venue: "Lorem Ipsum",
+      functional_area: 'Lorem Ipsum',
+      job_title: "Lorem Ipsum",
+      functional_requirements: ['Lorem', 'Ipsum'],
+      headcount: <InputNumber defaultValue={3} onChange={handleHeadcount}/>
+    },
+    {
+      key: "2",
+      venue: "Lorem Ipsum",
+      functional_area: 'Lorem Ipsum',
+      job_title: "Lorem Ipsum",
+      functional_requirements: ['Lorem', 'Ipsum'],
+      headcount: <InputNumber defaultValue={3}/>
+    },
+    {
+      key: "3",
+      venue: "Lorem Ipsum",
+      functional_area: 'Lorem Ipsum',
+      job_title: "Lorem Ipsum",
+      functional_requirements: ['Lorem', 'Ipsum'],
+      headcount: <InputNumber defaultValue={3}/>
+    },
+  ];
+
   return (
-    <div className="function-and-requirements-table">
-      <Table
-        columns={columns}
-        dataSource={data}
-      />
-    </div>
+    <>
+      <div className="function-and-requirements-table">
+        <Table
+          columns={columns}
+          dataSource={data}
+        />
+      </div>
+      <FunctionAndRequirementsModal isModalVisible={isModalVisible} setIsModalVisible={setIsModalVisible} />
+    </>
   );
 };
 
-export default FARequirementsTable;
+export default FunctionAndRequirementsTable;
