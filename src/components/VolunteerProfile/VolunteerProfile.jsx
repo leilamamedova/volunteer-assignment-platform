913b7f6c748d097105cf1f6c8dd251743fca6db9
Musa Musazada <musaam@code.edu.az>
2022-05-04 09:21:34 +0400

    ./AssignAndSearchResult Volunteer Modal Functionality Added to the Table

--- src/components/VolunteerProfile/VolunteerProfile.jsx
+++ src/components/VolunteerProfile/VolunteerProfile.jsx
@@ -1,45 +1,55 @@
-import { Space } from "antd";
+import { Space, Modal, Button } from "antd";
+
 import "./VolunteerProfile.scss";
 
-function VolunteerProfile() {
+function VolunteerProfile(props) {
   return (
-    <Space
-      direction="vertical"
-      size="large"
-      className="overflow-y--auto card volunteer--card"
+    <Modal
+      className="volunteer-profile--modal"
+      title="Details"
+      visible={props.isModalVisible}
+      onOk={props.handleOk}
+      onCancel={props.handleCancel}
+      width={700}
     >
-      <Space size="middle">
-        <span className="fs-xl">Role Match: </span>
-        <span className="perfect fs-xl bold">98%</span>
+      <Space
+        direction="vertical"
+        size="large"
+        className="overflow-y--auto card volunteer--card"
+      >
+        <Space size="middle">
+          <span className="fs-xl">Role Match: </span>
+          <span className="perfect fs-xl bold">98%</span>
+        </Space>
+        <Space direction="vertical" size="small">
+          <div className="fs-lg">
+            <span className="bold">ID</span>: #JKSD23
+          </div>
+          <i className="fs-lg">Jerome Willhenberg</i>
+        </Space>
+        <Space direction="vertical" size="small">
+          <div className="fs-lg">
+            <span className="bold">Age</span>: 36
+          </div>
+          <div className="fs-lg perfect">
+            <span className="bold">English</span>: Fluent
+          </div>
+          <div className="fs-lg ">
+            <span className="bold">Skills</span>: Customer Services, Ability to
+            handle difficult situations
+          </div>
+          <div className="fs-lg ">
+            <span className="bold">Interview Score</span>: 100
+          </div>
+          <div className="fs-lg perfect">
+            <span className="bold">Computer Proficiency</span>: High
+          </div>
+          <div className="fs-lg ">
+            <span className="bold">Residence Zone</span>: Doha
+          </div>
+        </Space>
       </Space>
-      <Space direction="vertical" size="small">
-        <div className="fs-lg">
-          <span className="bold">ID</span>: #JKSD23
-        </div>
-        <i className="fs-lg">Jerome Willhenberg</i>
-      </Space>
-      <Space direction="vertical" size="small">
-        <div className="fs-lg">
-          <span className="bold">Age</span>: 36
-        </div>
-        <div className="fs-lg perfect">
-          <span className="bold">English</span>: Fluent
-        </div>
-        <div className="fs-lg ">
-          <span className="bold">Skills</span>: Customer Services, Ability to
-          handle difficult situations
-        </div>
-        <div className="fs-lg ">
-          <span className="bold">Interview Score</span>: 100
-        </div>
-        <div className="fs-lg perfect">
-          <span className="bold">Computer Proficiency</span>: High
-        </div>
-        <div className="fs-lg ">
-          <span className="bold">Residence Zone</span>: Doha
-        </div>
-      </Space>
-    </Space>
+    </Modal>
   );
 }
 
