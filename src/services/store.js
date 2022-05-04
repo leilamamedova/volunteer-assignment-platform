9c95405f0c09eae426da6c687b647911a428ae78
Musa Musazada <musaam@code.edu.az>
2022-05-04 10:39:39 +0400

    ./AssigningTo Logic for Dynamic Filters Added

--- src/services/store.js
+++ src/services/store.js
@@ -1,8 +1,16 @@
-import create from 'zustand';
+import create from "zustand";
 
-const useStore = create(set => ({
-    functionalRequirements: [],
-    setFunctionalRequirements: (req) => set((state) => ({ functionalRequirements: req })),
-}))
+const useStore = create((set) => ({
+  functionalRequirements: [],
+  setFunctionalRequirements: (req) =>
+    set((state) => ({ functionalRequirements: req })),
+  //Filter Functional Requirements
+  filterRequirements: [],
+  setFilterRequirements: (req) =>
+    set(
+      (state) =>
+        (state.filterRequirements = [...state.filterRequirements, ...req])
+    ),
+}));
 
-export default useStore;     
+export default useStore;
