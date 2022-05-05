00b001ad66e326fd810b0bdc7c53c64026e73f31
leilamamedova <leila.m26ova@gmail.com>
2022-05-05 09:44:34 +0400

    create chart by gender on dashboard

---
+++ src/components/GenderChart/GenderChart.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Card } from 'antd';
+import './GenderChart.scss';
+import {PieChart} from "./components/Charts";
+
+const UserData = [
+    {
+      id: 1,
+      year: 'male',
+      userGain: 1
+    },
+    {
+      id: 2,
+      year: 'female',
+      userGain: 5
+    },
+  ];
+
+const GenderChart = () => {
+    const [userData, setUserData] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [
+          {
+            label: "Users Gained",
+            data: UserData.map((data) => data.userGain),
+            backgroundColor: [
+              "#2EB4E6",
+              "#E54A58",
+            ],
+            borderColor: "black",
+            borderWidth: 1,
+          },
+        ],
+      });
+
+    return (
+        <div style={{width: 200}} >
+            <PieChart chartData={userData} />
+        </div>        
+    );
+};
+
+export default GenderChart;
