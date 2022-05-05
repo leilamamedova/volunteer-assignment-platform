0059572f588ed6d8c876cbfe718217b5a7433524
leilamamedova <leila.m26ova@gmail.com>
2022-05-05 10:05:39 +0400

    create nationality chart on dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import GenderChart from '../../components/GenderChart/GenderChart';
+import GenderChart from '../../components/Charts/components/GenderChart/GenderChart';
+import NationalityChart from '../../components/Charts/components/NationalityChart/NationalityChart';
 import './Dashboard.scss';
 
 const Dashboard =  () => {
     return (
         <>
-            <GenderChart/>        
+            <GenderChart/>       
+            <NationalityChart/> 
         </>
     )
 };
