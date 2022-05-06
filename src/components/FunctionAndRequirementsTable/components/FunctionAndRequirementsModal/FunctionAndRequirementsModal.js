c84e4d256ab6324344a80e0bb8e802334ef98ade
leilamamedova <leila.m26ova@gmail.com>
2022-05-06 10:46:22 +0400

    create-bulk-import-button-on-func-req-page

--- src/components/FunctionAndRequirementsTable/components/FunctionAndRequirementsModal/FunctionAndRequirementsModal.js
+++ src/components/FunctionAndRequirementsTable/components/FunctionAndRequirementsModal/FunctionAndRequirementsModal.js
@@ -1,29 +1,9 @@
 import React from 'react';
-import { Modal, Upload, message, Button, Col, Row, Select, Tag } from 'antd';
-import { DownloadOutlined } from '@ant-design/icons';
+import { Modal, Col, Row, Select, Tag } from 'antd';
 import useStore from '../../../../services/store';
 import './FunctionAndRequirementsModal.scss';
 
 const { Option } = Select;
-
-const props = {
-    name: 'file',
-    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-    headers: {
-      authorization: 'authorization-text',
-    },
-    accept:".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel",
-    onChange(info) {
-      if (info.file.status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === 'done') {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === 'error') {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-};
 
 const FunctionAndRequirementsModal = ({isModalVisible, setIsModalVisible, selectedRow}) => {
     const functionalRequirements = useStore(({functionalRequirements})=>functionalRequirements);
@@ -51,18 +31,6 @@ const FunctionAndRequirementsModal = ({isModalVisible, setIsModalVisible, select
     return (
         <>
             <Modal centered closable={false} className='function-and-requirements-modal' visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                <Row className='import-requirements'>
-                    <Col span={8}>
-                        <h3>Import requirements</h3>
-                    </Col>
-
-                    <Col span={8} offset={8}>
-                        <Upload {...props}  maxCount={1}>
-                            <Button icon={<DownloadOutlined />}>Click to Import</Button>
-                        </Upload>
-                    </Col>
-                </Row>
-
                 <Row className='add-requirements'>
                     <Col span={8}>
                         <h3>Add requirements</h3>
