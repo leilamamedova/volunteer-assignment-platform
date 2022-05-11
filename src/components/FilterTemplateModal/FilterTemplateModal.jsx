b3a20edb9403364be2bfcd7ee201eb7058a93af4
Musa Musazada <musaam@code.edu.az>
2022-05-11 17:38:17 +0400

    Editing & Deleting Saved Template Filters are available

--- src/components/FilterTemplateModal/FilterTemplateModal.jsx
+++ src/components/FilterTemplateModal/FilterTemplateModal.jsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Modal } from "antd";
+import { Modal, Button } from "antd";
 import FilterWrapper from "../FilterWrapper/FilterWrapper";
+import useStore from "../../services/store";
 function FilterTemplateModal(props) {
+  const filterFields = useStore((state) => state.filterFields);
+  const favoriteFilters = useStore((state) => state.favoriteFilters);
   const handleOk = () => {
     props.setIsModalVisible(false);
   };
 
   const handleCancel = () => {
     props.setIsModalVisible(false);
+  };
+  const handleSave = () => {
+    favoriteFilters.forEach((el) => {
+      el.key === props.templateId
+        ? (el.filters = filterFields)
+        : alert("failed");
+    });
   };
   return (
     <Modal
@@ -18,7 +28,10 @@ function FilterTemplateModal(props) {
       onCancel={handleCancel}
       footer={[]}
     >
-      <FilterWrapper noReset={true} />
+      <FilterWrapper noReset={true} isFav={true} />
+      <Button className="mt-20" type="primary" onClick={handleSave}>
+        Save Changes
+      </Button>
     </Modal>
   );
 }
