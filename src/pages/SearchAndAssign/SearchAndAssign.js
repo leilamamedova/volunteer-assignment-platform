460762ff50953c33509ea37b7b327f791cefbaa0
Musa Musazada <musaam@code.edu.az>
2022-05-11 18:01:15 +0400

    ./SearchAndAssign.jsx Filter Template Load Component Added

--- src/pages/SearchAndAssign/SearchAndAssign.js
+++ src/pages/SearchAndAssign/SearchAndAssign.js
@@ -5,12 +5,13 @@ import AssignSearchResult from "../../components/AssignSearchResult/AssignSearch
 import VolunteerProfile from "../../components/VolunteerProfile/VolunteerProfile";
 import "./SearchAndAssign.scss";
 import ResultButton from "../../components/ResultButton/ResultButton";
+import LoadFilterTemplate from "../../components/LoadFilterTemplate/LoadFilterTemplate";
 
 const SearchAndAssign = () => {
   return (
     <div className="search-and-assign">
       <Space className="mb-20">
-        <Button type="primary">Load Search</Button>
+        <LoadFilterTemplate />
         <ResultButton />
       </Space>
       <Row gutter={15} wrap={true} justify="space-between">
