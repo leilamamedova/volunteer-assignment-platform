dcf99643ae0ddfdcfac611cbe527ea04e37f0f04
Musa Musazada <musaam@code.edu.az>
2022-05-12 11:11:33 +0400

    List of Filters functionality created

---
+++ src/components/FilterListModal/FilterListModal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Modal } from "antd";
+import "./FilterListModal.scss";
+
+const FunctionAndRequirementsModal = ({
+  isModalVisible,
+  setIsModalVisible,
+  filterList,
+}) => {
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+  return (
+    <>
+      <Modal
+        centered
+        closable={false}
+        className="function-list-modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        {filterList.map((item, index) => (
+          <p key={index} className="filter-list-text">
+            <span className="field">{item.field}</span>{" "}
+            <span className="operator">{item.comparison}</span>
+            <span className="value">
+              {item.value.trim() === "" ? "Empty" : item.value}
+            </span>
+          </p>
+        ))}
+      </Modal>
+    </>
+  );
+};
+
+export default FunctionAndRequirementsModal;
