4f47ab5d97a1644ba1d5dbc1f999d13c52dc1c3f
leilamamedova <leila.m26ova@gmail.com>
2022-05-12 12:08:21 +0400

    connect to users api

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -1,107 +1,38 @@
-import { useState } from "react";
-import { Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { Table, Typography } from "antd";
 import VolunteerProfile from "../VolunteerProfile/VolunteerProfile";
+import useStore from "../../services/store";
+import './UsersTable.scss';
 const { Link } = Typography;
-
-const data = [
-  {
-    key: "1",
-    name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-    status: "assigned",
-  },
-  {
-    key: "2",
-    name: "Jim Green",
-    age: 42,
-    address: "London No. 1 Lake Park",
-    status: "free",
-  },
-  {
-    key: "3",
-    name: "Joe Black",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "waitlist",
-  },
-  {
-    key: "4",
-    name: "Joe Doe",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "free",
-  },
-  {
-    key: "5",
-    name: "Jane Doe",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "waitlist",
-  },
-  {
-    key: "6",
-    name: "Simon Fitzel",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "assigned",
-  },
-  {
-    key: "7",
-    name: "Richard Brown",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "waitlist",
-  },
-  {
-    key: "8",
-    name: "Susan Liol",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "assigned",
-  },
-  {
-    key: "9",
-    name: "David Uollo",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-    status: "free",
-  },
-];
 
 const AssignSearchResult = () => {
   const [isVolunteerModalVisible, setIsVolunteerModalVisible] = useState(false);
+  const [columns, setColumns] = useState([]);
+  const [dataKeys, setDataKeys] = useState([]);
+  const usersData = useStore(({usersData}) => usersData);
+
   //Later id will fetch the active modal data.
   const showVolunteerModal = (id) => {
     setIsVolunteerModalVisible(true);
   };
 
-  const columns = [
-    {
-      title: "Name",
-      dataIndex: "name",
-      render: (_, field) => (
-        <Link onClick={() => showVolunteerModal(field.id)}>{field.name}</Link>
-      ),
-    },
-    {
-      title: "Age",
-      dataIndex: "age",
-    },
-    {
-      title: "Address",
-      dataIndex: "address",
-    },
-    {
-      title: "Status",
-      key: "status",
-      render: (_, field) => (
-        <Space className="action-icons" align="baseline">
-          <p>{field.status}</p>
-        </Space>
-      ),
-    },
-  ];
+  useEffect(() => {
+    usersData.length>0 && Object.keys(usersData[0]).map((item) => {
+      setDataKeys((prev) => [...prev,
+        { 
+          title: item.replaceAll('_', " "),
+          dataIndex: item,
+        }
+      ])
+    })    
+  }, [usersData])
+
+  useEffect(()=> {
+      dataKeys.length>0 && (dataKeys.find(el => el.dataIndex === 'first_name')['render'] = 
+        (data, record) => <Link onClick={() => showVolunteerModal(record.key)}>{data}</Link>    
+      )
+      setColumns(dataKeys);
+  },[dataKeys])
 
   const rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
@@ -120,8 +51,10 @@ const AssignSearchResult = () => {
           rowSelection={{
             ...rowSelection,
           }}
+          scroll={{ x: 240 }}
           columns={columns}
-          dataSource={data}
+          dataSource={usersData}
+          rowKey={record => record.uid}
         />
       </div>
       <VolunteerProfile
