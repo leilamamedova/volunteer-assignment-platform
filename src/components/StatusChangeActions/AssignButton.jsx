34a51be7e06ef7f7c2d9872608184d37d8b1a29f
Musa Musazada <musaam@code.edu.az>
2022-05-16 12:17:38 +0400

    Status Change Action Buttons Connected to Data and API

--- src/components/StatusChangeActions/AssignButton.jsx
+++ src/components/StatusChangeActions/AssignButton.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Button } from "antd";
 function AssignButton(props) {
+  const endpoint = `${process.env.REACT_APP_VAP_API_BASE}/Assignments/${props.route}`;
+
   const handleAssign = () => {
-    console.log("Assigning...");
-    console.log("--------------");
-    console.log(props.users);
+    const postData = props.data.map((el) =>
+      Object.assign({ id: el, role_offer_id: props.roleOfferId, status: 0 })
+    );
+    console.log(postData);
+    fetch(endpoint, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   };
+
   return (
     <Button type="primary" onClick={handleAssign}>
       Assign
