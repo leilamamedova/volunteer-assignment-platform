cdff314b53527797b8634dc52b0781c533ad36a1
Musa Musazada <musaam@code.edu.az>
2022-05-17 15:47:21 +0400

    Reset and Check/UnCheck all logic implemented on ColumnFilter

--- src/components/ColumnFilter/ColumnFilter.jsx
+++ src/components/ColumnFilter/ColumnFilter.jsx
@@ -1,24 +1,69 @@
-import React, { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import { Checkbox, Button } from "antd";
 import "./ColumnFilter.scss";
 function ColumnFilter({ columns, handleColumns }) {
   const [toggle, setToggle] = useState(false);
+  const plainOptions = columns.map((el) => el.title);
+  const [checkedList, setCheckedList] = useState([]);
+  const [indeterminate, setIndeterminate] = useState(true);
+  const [checkAll, setCheckAll] = useState(false);
+
+  //Listens for Checbox.Group component changes
+  const onChange = (list) => {
+    setCheckedList(list);
+    setIndeterminate(!!list.length && list.length < plainOptions.length);
+    setCheckAll(list.length === plainOptions.length);
+    handleChange(list);
+  };
+  //Listens for the Check/Uncheck
+  const onCheckAllChange = (e) => {
+    setCheckedList(e.target.checked ? plainOptions : []);
+    setIndeterminate(false);
+    setCheckAll(e.target.checked);
+    handleChange([]);
+  };
 
   const handleChange = (value) => {
     handleColumns(value);
   };
+
   const handleClick = () => {
     setToggle((prev) => !prev);
+  };
+  const reset = () => {
+    setCheckedList([]);
+    setIndeterminate(false);
+    setCheckAll(false);
+    handleChange([]);
   };
   return (
     <div className="column--list-wrapper">
       <Button className="mb-20" type="primary" onClick={handleClick}>
         {toggle ? "Hide" : "Filter Columns"}
       </Button>
+      {toggle ? (
+        <>
+          <Checkbox
+            className="ml-20"
+            indeterminate={indeterminate}
+            onChange={onCheckAllChange}
+            checked={checkAll}
+          >
+            Check All
+          </Checkbox>
+          <Button className="ml-20" type="primary" onClick={reset}>
+            Reset
+          </Button>
+        </>
+      ) : (
+        ""
+      )}
+
       <Checkbox.Group
         className={toggle ? "column--list" : "hide"}
-        options={columns.map((el) => el.title)}
-        onChange={handleChange}
+        options={plainOptions}
+        value={checkedList}
+        onChange={onChange}
       />
     </div>
   );
