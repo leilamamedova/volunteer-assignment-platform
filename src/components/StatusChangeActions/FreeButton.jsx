27f3240d561c973b2a3d3b0c4d0ab5ebe6782d12
Musa Musazada <musaam@code.edu.az>
2022-05-19 16:58:33 +0400

    Changing Status depends on ActiveRoleOffer Id

--- src/components/StatusChangeActions/FreeButton.jsx
+++ src/components/StatusChangeActions/FreeButton.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Button } from "antd";
 import useStore from "../../services/store";
-import { UsersFetch, UsersFieldsFetch } from "../../services/fetch";
+import { UsersFetch } from "../../services/fetch";
 
 function FreeButton(props) {
   const setUsersData = useStore(({ setUsersData }) => setUsersData);
   const setTableLoading = useStore(({ setTableLoading }) => setTableLoading);
-  const setUsersDataFields = useStore(
-    ({ setUsersDataFields }) => setUsersDataFields
+  const activeRoleOfferId = useStore(
+    ({ activeRoleOfferId }) => activeRoleOfferId
   );
+  const [isDisabled, setIsDisabled] = useState(true);
   const endpoint = `${process.env.REACT_APP_VAP_API_BASE}/Assignments/${props.route}`;
+
+  useEffect(() => {
+    activeRoleOfferId === 0 ? setIsDisabled(true) : setIsDisabled(false);
+
+    return () => setIsDisabled(true);
+  }, [activeRoleOfferId]);
 
   const handleFree = () => {
     const postData = props.data.map((el) =>
-      Object.assign({ id: el, role_offer_id: props.roleOfferId, status: 2 })
+      Object.assign({ id: el, role_offer_id: activeRoleOfferId, status: 2 })
     );
     console.log(postData);
     fetch(endpoint, {
@@ -31,7 +38,7 @@ function FreeButton(props) {
       .catch((err) => console.log(err));
   };
   return (
-    <Button className="green-bg" onClick={handleFree}>
+    <Button disabled={isDisabled} className="green-bg" onClick={handleFree}>
       Free
     </Button>
   );
--- src/components/StatusChangeActions/WaitlistButton.jsx
+++ src/components/StatusChangeActions/WaitlistButton.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Button } from "antd";
 import useStore from "../../services/store";
-import { UsersFetch, UsersFieldsFetch } from "../../services/fetch";
+import { UsersFetch } from "../../services/fetch";
 function WaitlistButton(props) {
   const setUsersData = useStore(({ setUsersData }) => setUsersData);
-  const setUsersDataFields = useStore(
-    ({ setUsersDataFields }) => setUsersDataFields
+  const activeRoleOfferId = useStore(
+    ({ activeRoleOfferId }) => activeRoleOfferId
   );
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const endpoint = `${process.env.REACT_APP_VAP_API_BASE}/Assignments/${props.route}`;
   const setTableLoading = useStore(({ setTableLoading }) => setTableLoading);
 
-  const handleWaitlist = () => {
+  useEffect(() => {
+    activeRoleOfferId === 0 ? setIsDisabled(true) : setIsDisabled(false);
 
+    return () => setIsDisabled(true);
+  }, [activeRoleOfferId]);
+
+  const handleWaitlist = () => {
     const postData = props.data.map((el) =>
-      Object.assign({ id: el, role_offer_id: props.roleOfferId, status: 1 })
+      Object.assign({ id: el, role_offer_id: activeRoleOfferId, status: 1 })
     );
     console.log(postData);
     fetch(endpoint, {
@@ -27,13 +33,18 @@ function WaitlistButton(props) {
     })
       .then((response) => {
         console.log(response);
-        UsersFetch(setUsersData,setTableLoading );
+        UsersFetch(setUsersData, setTableLoading);
       })
       .catch((err) => console.log(err));
   };
 
   return (
-    <Button type="primary" onClick={handleWaitlist} danger>
+    <Button
+      disabled={isDisabled}
+      type="primary"
+      onClick={handleWaitlist}
+      danger
+    >
       Waitlist
     </Button>
   );
