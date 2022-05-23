185afdca4748dcbc1012f8dbd8ee1b18d2044c66
Musa Musazada <musaam@code.edu.az>
2022-05-23 12:06:33 +0400

    Role Offer Id removed from Free Button

--- src/components/StatusChangeActions/FreeButton.jsx
+++ src/components/StatusChangeActions/FreeButton.jsx
@@ -1,25 +1,15 @@
-import { useState, useEffect } from "react";
 import { Button } from "antd";
 import useStore from "../../services/store";
 import { UsersFetch } from "../../services/fetch";
 
 function FreeButton(props) {
   const setUsersData = useStore(({ setUsersData }) => setUsersData);
-  const activeRoleOfferId = useStore(
-    ({ activeRoleOfferId }) => activeRoleOfferId
-  );
-  const [isDisabled, setIsDisabled] = useState(true);
+
   const endpoint = `${process.env.REACT_APP_VAP_API_BASE}/Assignments/${props.route}`;
-
-  useEffect(() => {
-    activeRoleOfferId === 0 ? setIsDisabled(true) : setIsDisabled(false);
-
-    return () => setIsDisabled(true);
-  }, [activeRoleOfferId]);
 
   const handleFree = () => {
     const postData = props.data.map((el) =>
-      Object.assign({ id: el, role_offer_id: activeRoleOfferId, status: 2 })
+      Object.assign({ id: el, status: 2 })
     );
     console.log(postData);
     fetch(endpoint, {
@@ -37,7 +27,7 @@ function FreeButton(props) {
       .catch((err) => console.log(err));
   };
   return (
-    <Button disabled={isDisabled} className="green-bg" onClick={handleFree}>
+    <Button className="green-bg" onClick={handleFree}>
       Free
     </Button>
   );
