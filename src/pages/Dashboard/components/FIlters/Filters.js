0bec87298b3dd86dfc9d54421058dd077f5f328d
leilamamedova <leila.m26ova@gmail.com>
2022-06-07 16:22:10 +0400

    fix dashboard filter

--- src/pages/Dashboard/components/FIlters/Filters.js
+++ src/pages/Dashboard/components/FIlters/Filters.js
@@ -69,6 +69,16 @@ const Filters = ({showUserData=false}) => {
     setEntities(entityData);
   }, [roleOffers]);
 
+  useEffect(() => {
+    entity.length > 0 && setIsFADisabled(false);
+    entity.length > 0  && functionalArea.length > 0 && setIsJobTitleDisabled(false);
+    entity.length > 0  && functionalArea.length > 0 && jobTitle.length > 0 && setIsVenueDisabled(false);
+    !showUserData && entity.length > 0  && functionalArea.length > 0 && jobTitle.length > 0 && venue.length > 0 && setSubmitDisabled(false);
+    showUserData && entity.length > 0  && functionalArea.length > 0 && jobTitle.length > 0 && venue.length > 0 && setStatusDisabled(false);
+    entity.length > 0  && functionalArea.length > 0 && jobTitle.length > 0 && venue.length > 0 && status.length > 0 && setLocatiobDisabled(false);
+    entity.length > 0  && functionalArea.length > 0 && jobTitle.length > 0 && venue.length > 0 && status.length > 0 && location.length > 0 && setSubmitDisabled(false)
+  }, [entity, functionalArea, jobTitle, location, venue, status, showUserData])
+
   //Submit Handler Logic
   const handleSubmit = (e) => {
     const data = {
@@ -109,22 +119,14 @@ const Filters = ({showUserData=false}) => {
     let functionalAreaData = [];
     value.forEach(value => { functionalAreaData.push(roleOffers.find(c => c.name === value)) })
     let functionalAreaMerged = removeDuplicateObjectFromArray([].concat.apply([],  functionalAreaData.map(item => item.functionalAreas)))
-
-    setSubmitDisabled(true);
-    setIsFADisabled(false);
     setFunctionalAreas(functionalAreaMerged);
   };
-
 
   const handleFAChange = (value) => { 
     setFunctionalArea(value)
     let jobTitleData = [];
     value.forEach(value => { jobTitleData.push(functionalAreas.find(c => c.name === value)) })
     let jobTitleMerged = removeDuplicateObjectFromArray([].concat.apply([],  jobTitleData.map(item => item.jobTitles)))
-
-    setSubmitDisabled(true);
-    setIsJobTitleDisabled(false);
-    setIsVenueDisabled(true);
     setJobTitles(jobTitleMerged);
   };
   const handleJobTitleChange = (value) => {
@@ -132,9 +134,6 @@ const Filters = ({showUserData=false}) => {
     let venueData = [];
     value.forEach(value => { venueData.push(jobTitles.find(c => c.name === value)) })
     let venueMerged = removeDuplicateObjectFromArray([].concat.apply([],  venueData.map(item => item.locations)))
-
-    setSubmitDisabled(true);
-    setIsVenueDisabled(false);
     setVenues(venueMerged);
   };
   const handleVenueChange = (value) => {
@@ -142,19 +141,15 @@ const Filters = ({showUserData=false}) => {
     let roleId = [];
     value.forEach(value => { roleId.push(venues.find(c => c.name === value)) });
     const idList = roleId.map(el => el.roleOffer.id)
-
     setRoleOfferId(idList);
-    showUserData ? setStatusDisabled(false) : setSubmitDisabled(false);
   };
 
   const handleStatusChange = (value) => {
     setStatus(value);
-    setLocatiobDisabled(false);
   };
 
   const handleLocationChange = (value) => {
     setLocation(value);
-    setSubmitDisabled(false);
   };
 
   const selectAllEntity = (e) => {
